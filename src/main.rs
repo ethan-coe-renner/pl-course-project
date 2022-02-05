4a9c880931193e47f0f699fcf733ea2b9135d660
Ethan Coe-Renner <e.coerenner@gmail.com>
2022-02-05 02:12:24 -0800

    initial commit, finish part 1

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+mod scanner;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let inputpath = Path::new(&args[1]);
+    let display = inputpath.display();
+
+    let mut inputfile = match File::open(&inputpath) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut input_text = String::new();
+    match inputfile.read_to_string(&mut input_text) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => println!("Successfully read file\n"),
+    }
+
+    let tokens = scanner::parse_file(input_text);
+    let token_string: String = tokens.iter().map(|token| token.to_string()).collect::<Vec<String>>().join("\n");
+
+    let outputpath = Path::new(&args[2]);
+    let display = outputpath.display();
+
+    let mut output_file = match File::create(&outputpath) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    match output_file.write_all(token_string.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+}
---
+++ src/scanner.rs
@@ -0,0 +1,99 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::fmt;
+
+#[derive(PartialEq, Eq, Hash, Debug)]
+pub enum Token {
+    Identifier,
+    Number,
+    Symbol,
+}
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+pub fn parse_file(file: String) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    for line in file.lines() {
+        println!("Line: {}", line);
+        tokens.append(&mut line_to_tokens(line));
+        println!("");
+    }
+    tokens
+}
+
+fn line_to_tokens(line: &str) -> Vec<Token> {
+    let mut regex_map = HashMap::new(); //map from Token to regex matching that token
+    regex_map.insert(
+        Token::Identifier,
+        Regex::new(r"^([[:alpha:]])([[:alpha:]]|[0-9])*$").unwrap(),
+    );
+    regex_map.insert(Token::Number, Regex::new(r"^[0-9]+$").unwrap());
+    regex_map.insert(Token::Symbol, Regex::new(r"^(\+|\-|\*|/|\(|\))$").unwrap());
+
+    let words = line.split_whitespace();
+    let mut tokens = Vec::new();
+
+    for word in words {
+        let mut buffer = String::new(); // buffer of current match
+        let mut chars = word.chars().peekable();
+        let mut next = chars.next();
+        buffer.push(next.unwrap());
+        let mut token = match assign_initial_token(&buffer, &regex_map) {
+            None => {
+                println!("Error reading \"{}\"", buffer);
+                return Vec::new();
+            }
+            Some(t) => t,
+        };
+
+        next = match chars.peek() {
+            None => None,
+            Some(&c) => Some(c),
+        };
+
+        while next != None {
+            buffer.push(next.unwrap());
+            if regex_map.get(&token).unwrap().is_match(&buffer) {
+                chars.next(); // we can go to next character because this char works
+            } else {
+                // reached end of token
+                buffer.pop();
+                println!("{}: {:?}", buffer, &token);
+                tokens.push(token);
+                buffer = String::new();
+                next = chars.next();
+                buffer.push(next.unwrap());
+                token = match assign_initial_token(&buffer, &regex_map) {
+                    None => {
+                        println!("Error reading \"{}\"", buffer);
+                        return Vec::new();
+                    }
+                    Some(t) => t,
+                };
+            }
+            next = match chars.peek() {
+                None => None,
+                Some(&c) => Some(c),
+            };
+        }
+        println!("{}: {:?}", buffer, &token);
+        tokens.push(token);
+    }
+    tokens
+}
+
+fn assign_initial_token(buffer: &str, regex_map: &HashMap<Token, Regex>) -> Option<Token> {
+    if regex_map.get(&Token::Identifier).unwrap().is_match(buffer) {
+        Some(Token::Identifier)
+    } else if regex_map.get(&Token::Number).unwrap().is_match(buffer) {
+        Some(Token::Number)
+    } else if regex_map.get(&Token::Symbol).unwrap().is_match(buffer) {
+        // TODO: add token to tokens and clear buffer
+        Some(Token::Symbol)
+    } else {
+        None
+    }
+}
