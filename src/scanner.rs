27ae134ec2ec63e7f22bfbc740aadfa2a1b9babe
Ethan Coe-Renner <e.coerenner@gmail.com>
2022-02-06 15:32:47 -0800

    modify structure of output file

--- src/scanner.rs
+++ src/scanner.rs
@@ -3,13 +3,24 @@ use std::collections::HashMap;
 use std::fmt;
 
 #[derive(PartialEq, Eq, Hash, Debug)]
-pub enum Token {
+enum TokenType {
     Identifier,
     Number,
     Symbol,
 }
 
+pub struct Token {
+    kind: TokenType,
+    value: String
+}
+
 impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.value, self.kind)
+    }
+}
+
+impl fmt::Display for TokenType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self)
     }
@@ -29,13 +40,13 @@ pub fn parse_file(file: String) -> Vec<Token> {
 fn line_to_tokens(line: &str) -> Vec<Token> {
     let mut regex_map = HashMap::new(); //map from Token to regex matching that token
     regex_map.insert(
-        Token::Identifier,
+        TokenType::Identifier,
         Regex::new(r"^([[:alpha:]])([[:alpha:]]|[0-9])*$").unwrap(),
     );
-    regex_map.insert(Token::Number, Regex::new(r"^[0-9]+$").unwrap());
-    regex_map.insert(Token::Symbol, Regex::new(r"^(\+|\-|\*|/|\(|\))$").unwrap());
+    regex_map.insert(TokenType::Number, Regex::new(r"^[0-9]+$").unwrap());
+    regex_map.insert(TokenType::Symbol, Regex::new(r"^(\+|\-|\*|/|\(|\))$").unwrap());
 
-    let mut tokens = Vec::new();
+    let mut tokens: Vec<Token> = Vec::new();
 
     for word in line.split_whitespace() {
         let mut buffer = String::new(); // buffer of current match
@@ -63,7 +74,7 @@ fn line_to_tokens(line: &str) -> Vec<Token> {
                 // reached end of token
                 buffer.pop();
                 println!("{}: {:?}", buffer, &token);
-                tokens.push(token);
+                tokens.push(Token {kind: token, value: buffer});
                 buffer = String::new();
                 next = chars.next();
                 buffer.push(next.unwrap());
@@ -81,20 +92,20 @@ fn line_to_tokens(line: &str) -> Vec<Token> {
             };
         }
         println!("{}: {:?}", buffer, &token);
-        tokens.push(token);
+        tokens.push(Token {kind: token, value: buffer});
     }
     tokens
 }
 
 // takes a char (ussually will only have one character) and finds the token which chould start with that buffer
 // If none match, returns None
-fn assign_initial_token(buffer: &str, regex_map: &HashMap<Token, Regex>) -> Option<Token> {
-    if regex_map.get(&Token::Identifier).unwrap().is_match(buffer) {
-        Some(Token::Identifier)
-    } else if regex_map.get(&Token::Number).unwrap().is_match(buffer) {
-        Some(Token::Number)
-    } else if regex_map.get(&Token::Symbol).unwrap().is_match(buffer) {
-        Some(Token::Symbol)
+fn assign_initial_token(buffer: &str, regex_map: &HashMap<TokenType, Regex>) -> Option<TokenType> {
+    if regex_map.get(&TokenType::Identifier).unwrap().is_match(buffer) {
+        Some(TokenType::Identifier)
+    } else if regex_map.get(&TokenType::Number).unwrap().is_match(buffer) {
+        Some(TokenType::Number)
+    } else if regex_map.get(&TokenType::Symbol).unwrap().is_match(buffer) {
+        Some(TokenType::Symbol)
     } else {
         None
     }
